e5b02941cc4f6c5a81e49643dd9c05087c1cd0b9
RavenaAntonelli <87223198+RavenaAntonelli@users.noreply.github.com>
2024-03-28 16:01:36 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python
+
+```
+docker build -t meu_projeto .
+```
+```
+docker run -it meu_projeto /bin/bash
+```
+```
+python script.py
+```
---
+++ script.py
@@ -0,0 +1,7 @@
+def main():
+    print("Olá, mundo! Este é um arquivo de teste Python.")
+
+if __name__ == "__main__":
+    main()
+
+# docker build -t meu_projeto .
